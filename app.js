d0ff183ca2fa094ab70808c9c3271ac212ebcd19
EWART-boop <johnewartdelacruz@gmail.com>
2024-09-08 14:44:31 +0800

    act number1

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log("HELLO WORLD");
+let myName = "John Eart Dela Cruz";
+console.log("My name is", myName);
+
+
+console.log("Using Variables");
+const age = "19";
+const name = "J.E Dela Cruz";
+console.log("My age is", age , "year old");console
+console.log("My name is", name);
+
+
+console.log("Using Numbers");
+const number1 = 26;
+const number2 = 15;
+const sub = number1 - number2;
+console.log(sub);
+
+
+console.log("Using booleans");
+const X = 71;
+const Y = 65;
+const Greater = X > Y;
+const less = X < Y;
+const equal = X == Y;
+console.log(Greater);
+console.log(less);
+console.log(equal);
+
+console.log("Comparison Operators");
+const temp = 20;
+if(temp > 0 && temp <= 30){
+    console.log("The Weather is Good");
+}
+else{
+    console.log("The Weather is Bad");
+}
+
